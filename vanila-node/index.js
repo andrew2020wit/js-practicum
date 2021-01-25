601c5412652080212af1c48a4ec7a46fc1e4478b
andrew2020wit <andrew2020wit@gmail.com>
2021-01-25 14:19:08 +0200

    clear

--- vanila-node/index.js
+++ vanila-node/index.js
@@ -1,25 +1,4 @@
-// Promise
-
-const boolean1 = false;
-
+// regexp
 console.log("=== start ===", new Date().toLocaleTimeString());
-
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (boolean1) {
-      resolve("someValue");
-    } else {
-      reject("errrror");
-    }
-  }, 200);
-});
-
-promise1
-  .then((x) => {
-    console.log("promise1.then:", x);
-  })
-  .catch((x) => {
-    console.log("promise1.catch:", x);
-  });
 
 console.log("=== end ===");
